0e2bacc4d66fd9c9466b15476364930dc41e7a45
Kemal Gökay <ketopal@stu.okan.edu.tr>
2017-11-04 11:58:25 +0300

    Add files via upload

---
+++ Main2Activity.java
@@ -0,0 +1,15 @@
+package com.rootsoft.endareco.utils;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.rootsoft.endareco.R;
+
+public class Main2Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
